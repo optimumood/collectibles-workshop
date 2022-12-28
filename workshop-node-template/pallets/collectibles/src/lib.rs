737dc5e01a6665c0f18fd7ac195daa9040533a88
optimumood <optimumood@gmail.com>
2022-12-28 22:07:18 +0100

    Added custom storage items to collectibles pallet

--- workshop-node-template/pallets/collectibles/src/lib.rs
+++ workshop-node-template/pallets/collectibles/src/lib.rs
@@ -1,19 +1,26 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_support::{
+	pallet_prelude::{
+		Decode, Encode, MaxEncodedLen, RuntimeDebug, StorageMap, StorageValue, TypeInfo,
+	},
+	traits::{Currency, Randomness},
+};
 pub use pallet::*;
-use frame_support::{pallet_prelude::{Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen}, traits::{Currency, Randomness}};
 
-type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+type BalanceOf<T> =
+	<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
 #[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 pub enum Color {
 	Red,
 	Yellow,
 	Blue,
-	Green
+	Green,
 }
 
 #[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+#[scale_info(skip_type_params(T))]
 pub struct Collectible<T: Config> {
 	// Unsigned integers of 16 bytes to represent a unique identifier
 	pub uniqueId: [u8; 16],
@@ -29,16 +36,34 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
-    pub struct Pallet<T>(_);
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
+	#[pallet::storage]
+	pub(super) type CollectiblesCount<T: Config> = StorageValue<_, u64, ValueQuery>;
+
+	/// Maps the Collectible struct to the unique_id.
+	#[pallet::storage]
+	pub(super) type CollectibleMap<T: Config> =
+		StorageMap<_, Twox64Concat, [u8; 16], Collectible<T>>;
+
+	/// Track the collectibles owned by each account.
+	#[pallet::storage]
+	pub(super) type OwnerOfCollectibles<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		T::AccountId,
+		BoundedVec<[u8; 16], T::MaximumOwned>,
+		ValueQuery,
+	>;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
 		type Currency: Currency<Self::AccountId>;
 		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
 
 		#[pallet::constant]
 		type MaximumOwned: Get<u32>;
-    }
+	}
 }
