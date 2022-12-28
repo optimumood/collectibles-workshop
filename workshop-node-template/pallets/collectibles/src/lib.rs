a64255cd05f4bbc0b3d92a19d59e6a1f49580159
optimumood <optimumood@gmail.com>
2022-12-28 03:15:49 +0100

    Added custom types to collectibles pallet

--- workshop-node-template/pallets/collectibles/src/lib.rs
+++ workshop-node-template/pallets/collectibles/src/lib.rs
@@ -1,11 +1,33 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
+use frame_support::{pallet_prelude::{Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen}, traits::{Currency, Randomness}};
+
+type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+pub enum Color {
+	Red,
+	Yellow,
+	Blue,
+	Green
+}
+
+#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+pub struct Collectible<T: Config> {
+	// Unsigned integers of 16 bytes to represent a unique identifier
+	pub uniqueId: [u8; 16],
+	// `None` assumes not for sale
+	pub price: Option<BalanceOf<T>>,
+	pub color: Color,
+	pub owner: T::AccountId,
+}
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::pallet_prelude::*;
-    use frame_system::pallet_prelude::*;
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
@@ -13,5 +35,10 @@ pub mod pallet {
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
+		type Currency: Currency<Self::AccountId>;
+		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
+
+		#[pallet::constant]
+		type MaximumOwned: Get<u32>;
     }
 }
